64452d9ad4185a4e73895e785443a2002ca1d5d2
Oscar Andersson <oscar.andersson.200@gmail.com>
2020-04-02 20:18:04 +0200

    added types to all vars

--- view.js
+++ view.js
@@ -1,42 +1,160 @@
+"use strict";
+exports.__esModule = true;
+var vue_1 = require("vue");
 // Datastructures
-var page = /** @class */ (function () {
-    function page(hero, cape) {
+var Page = /** @class */ (function () {
+    function Page(hero, cape) {
         this.hero = hero;
         this.cape = cape;
     }
-    Object.defineProperty(page.prototype, "print", {
-        get: function () {
-            return '<comp-hero hero=hero /><comp-cape cape=cape />';
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return page;
+    return Page;
 }());
-var hero = /** @class */ (function () {
-    function hero(title, subtitles) {
+var Hero = /** @class */ (function () {
+    function Hero(title, subtitles) {
         this.title = title;
         this.subtitles = subtitles;
     }
-    return hero;
+    return Hero;
 }());
-var cape = /** @class */ (function () {
-    function cape(cards) {
+var Cape = /** @class */ (function () {
+    function Cape(cards) {
         this.cards = cards;
     }
-    return cape;
+    return Cape;
 }());
-var subtititle = /** @class */ (function () {
-    function subtititle(text, click) {
-        this.text = text;
-        this.click = click;
-    }
-    return subtititle;
-}());
-var card = /** @class */ (function () {
-    function card(subtitles, imgsrc) {
-        this.imgsrc = imgsrc;
+var Subtitles = /** @class */ (function () {
+    function Subtitles(subtitles) {
         this.subtitles = subtitles;
     }
-    return card;
+    return Subtitles;
 }());
+var Subtitle = /** @class */ (function () {
+    function Subtitle(text, click, url) {
+        this.text = text;
+        this.click = click;
+        this.url = url;
+    }
+    return Subtitle;
+}());
+var Cards = /** @class */ (function () {
+    function Cards(cards) {
+        this.cards = cards;
+    }
+    return Cards;
+}());
+var Card = /** @class */ (function () {
+    function Card(image, subtitles) {
+        this.image = image;
+        this.subtitles = subtitles;
+    }
+    return Card;
+}());
+// Define components
+vue_1["default"].component('comp-page', {
+    props: {
+        hero: Hero,
+        cape: Cape
+    },
+    template: '<comp-hero hero=hero /><comp-cape cape=cape />'
+});
+vue_1["default"].component('comp-hero', {
+    props: {
+        title: String,
+        subtitles: Subtitles
+    },
+    template: '<div class=".hero"><h1>{{ title }}</h1><comp-subtitle subtitles=subtitles /><comp-cape/><svg class="arrow" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg></div>'
+});
+vue_1["default"].component('comp-subtitle', {
+    props: {
+        subtitle: String,
+        click: Function
+    },
+    template: '<div class="cape"><ul><li class="card" v-for="card in cards"><card-item card=card /></li></ul></div>'
+});
+vue_1["default"].component('comp-cape', {
+    props: {
+        cards: Cards
+    },
+    template: '<ul><li class="card" v-for="card in cards"><p>{{subtitle.p}}</p></li></ul>'
+});
+vue_1["default"].component('comp-card-item', {
+    props: {
+        image: String,
+        subtitles: Subtitles
+    },
+    template: '<div class="card-item"><img src="{{ image }}"/></div>'
+});
+// Navigation
+function navigate(app) {
+    console.log("Navigate to: " + app);
+    // Disable all
+    app_home.$data.seen = false;
+    app_contact.$data.seen = false;
+    app_key.$data.seen = false;
+    app_it.$data.seen = false;
+    app_dj.$data.seen = false;
+    // Enable one
+    app.seen = true;
+}
+function ShowHome() {
+    navigate(app_home);
+}
+function ShowContact() {
+    navigate(app_contact);
+}
+function ShowKey() { navigate(app_key); }
+function ShowIt() { navigate(app_it); }
+function ShowDj() { navigate(app_dj); }
+function redirect(url) { window.location.href = url; }
+// DEFINE HOME VIEW
+var page_home_cape_card_hithub = new Card("github.com", new Subtitles([
+    new Subtitle("Computer Engineering Student", null, null)
+]));
+var page_home_cape = new Cape([
+    page_home_cape_card_hithub
+]);
+var page_home_hero_subtitles = new Subtitles([
+    new Subtitle("Computer Engineering Student", ShowIt, null),
+    new Subtitle(".NET Developer", ShowIt, null),
+    new Subtitle("Internet freedom and privacy advocate", null, null),
+    new Subtitle("DJ", ShowDj, null)
+]);
+var page_home_hero = new Hero("Oscar Andersson", page_home_hero_subtitles);
+var page_home = new Page(page_home_hero, page_home_cape);
+// Initiate views
+var app_home = new vue_1["default"]({
+    el: '#app_home',
+    data: {
+        seen: true,
+        page: page_home
+    }
+});
+var app_contact = new vue_1["default"]({
+    el: '#app_contact',
+    data: {
+        seen: true,
+        page: page_home
+    }
+});
+var app_key = new vue_1["default"]({
+    el: '#app_key',
+    data: {
+        seen: true,
+        page: page_home
+    }
+});
+var app_dj = new vue_1["default"]({
+    el: '#app_dj',
+    data: {
+        seen: true,
+        page: page_home
+    }
+});
+var app_it = new vue_1["default"]({
+    el: '#app_it',
+    data: {
+        seen: true,
+        page: page_home
+    }
+});
+ShowHome();
--- view.ts
+++ view.ts
@@ -19,7 +19,7 @@ class Hero {
 }
 class Cape {
     public cards: Array<Card>;
-    public constructor(cards){
+    public constructor(cards: Array<Card>){
         this.cards = cards;
     }
 }
@@ -32,21 +32,23 @@ class Subtitles{
 class Subtitle {
     public text: String;
     public click: Function;
-    public constructor(text, click){
+    public url: String;
+    public constructor(text: String, click: Function, url: String){
         this.text = text;
-        this.click = click
+        this.click = click;
+        this.url = url;
     }
 }
 class Cards {
     public cards: Array<Card>;
-    public constructor(cards){
+    public constructor(cards: Array<Card>){
         this.cards = cards;
     }
 }
 class Card {
     public image: String;
-    public subtitles: Array<Subtitle>;
-    constructor(subtitles, image){
+    public subtitles: Subtitles;
+    constructor(image: String ,subtitles: Subtitles){
         this.image = image;
         this.subtitles = subtitles;
     }
@@ -88,36 +90,16 @@ Vue.component('comp-card-item', {
     template: '<div class="card-item"><img src="{{ image }}"/></div>'
 })
 
-// Initiate views
-let app_home = new Vue({
-    el: '#app_home',
-    data: page_home
-})
-let app_contact = new Vue({
-    el: '#app_contact',
-    data: null
-})
-let app_key = new Vue({
-    el: '#app_key',
-    data: null
-})
-let app_dj = new Vue({
-    el: '#app_dj',
-    data: null
-})
-let app_it = new Vue({
-    el: '#app_it',
-    data: null
-})
-
 // Navigation
 function navigate(app): void {
     console.log("Navigate to: " + app);
-    app_home.seen = false;
-    app_contact.seen = false;
-    app_key.seen = false;
-    app_it.seen = false;
-    app_dj.seen = false;
+    // Disable all
+    app_home.$data.seen = false;
+    app_contact.$data.seen = false;
+    app_key.$data.seen = false;
+    app_it.$data.seen = false;
+    app_dj.$data.seen = false;
+    // Enable one
     app.seen = true;
 }
 function ShowHome(): void {
@@ -132,34 +114,56 @@ function ShowDj(): void { navigate(app_dj) }
 function redirect(url): void { window.location.href = url }
 
 // DEFINE HOME VIEW
+let page_home_cape_card_hithub: Card = new Card("github.com", new Subtitles([
+    new Subtitle("Computer Engineering Student", null, null)
+]));
+let page_home_cape: Cape = new Cape([
+    page_home_cape_card_hithub
+]);
 let page_home_hero_subtitles: Subtitles = new Subtitles([
-    new Subtitle("Computer Engineering Student", ShowIt()),
-    new Subtitle(".NET Developer", ShowIt()),
-    new Subtitle("Internet freedom and privacy advocate", null),
-    new Subtitle("DJ", ShowDj())
+    new Subtitle("Computer Engineering Student", ShowIt, null),
+    new Subtitle(".NET Developer", ShowIt, null),
+    new Subtitle("Internet freedom and privacy advocate", null, null),
+    new Subtitle("DJ", ShowDj, null)
 ]);
 let page_home_hero: Hero = new Hero("Oscar Andersson", page_home_hero_subtitles)
-let page_home: Page = new Page();
+let page_home: Page = new Page(page_home_hero, page_home_cape);
 
-
-
-
-{
-    page: {
-        cape: {
-            cards: [
-                {
-                    imgsrc: "github.png",
-                    subtitles: [
-                        {
-                            text: "See my GitHub activity",
-                            click: redirect("https://github.com/oscar230/")
-                        },{
-                            text: "aaaaa"
-                        }
-                    ]
-                }
-            ]
-        }
+// Initiate views
+let app_home: Vue = new Vue({
+    el: '#app_home',
+    data: {
+        seen: true,
+        page: page_home
     }
-}
+})
+let app_contact = new Vue({
+    el: '#app_contact',
+    data: {
+        seen: true,
+        page: page_home
+    }
+})
+let app_key = new Vue({
+    el: '#app_key',
+    data: {
+        seen: true,
+        page: page_home
+    }
+})
+let app_dj = new Vue({
+    el: '#app_dj',
+    data: {
+        seen: true,
+        page: page_home
+    }
+})
+let app_it = new Vue({
+    el: '#app_it',
+    data: {
+        seen: true,
+        page: page_home
+    }
+})
+
+ShowHome();
