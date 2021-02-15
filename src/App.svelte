6ef4f7179acf58ec8132739608af3683736c963c
Oscar Andersson <oscar.andersson.200@gmail.com>
2021-02-15 19:16:50 +0100

    Presentation work.

--- src/App.svelte
+++ src/App.svelte
@@ -4,16 +4,11 @@
 
 <main>
 	<Presentation 
-		title="AAAAOscar Anderson" 
-		imgSrc="https://upload.wikimedia.org/wikipedia/commons/9/9b/Equinozio_da_Pizzo_Vento%2Ctramonto_fondachelli_fantina%2C_sicilia.JPG" 
-		imgAlt="Blue night sky over a mountain." />
+		title="OSCAR" 
+		imgSrc="https://i.imgur.com/zP3HGfR.jpeg" 
+		imgAlt="Blue sky over an old bridge in Karlstad, Sweden." />
 </main>
 
 <style>
-	main {
-		margin: 0;
-		padding: 2em;
-		background-color: #2f3640;
-		height: fit-content;
-	}
+
 </style>
--- src/Presentation.svelte
+++ src/Presentation.svelte
@@ -5,23 +5,82 @@
 </script>
 
 <style>
+
     div {
         margin: auto;
-		width: 50%;
-    }
-    h1 {
+        width: 60vw;
         position: relative;
-        top: -5em;
-        left: -5em;
-        font-size: larger;
+        top: 10vh;
     }
-    img {
+
+    @keyframes anim-letter-spacing {
+        0%   {letter-spacing: 0.8em;}
+        100% {letter-spacing: 0.7em;}
+    }
+
+    .title {
+        display: block;
+        position: relative;
+        top: 48vh;
+        left: -8vw;
+        font-size: 7vw;
+        text-align: center;
+        color: #d2dae2;
+        letter-spacing: 0.7em;
+        animation-name: anim-letter-spacing;
+        animation-duration: 3s;
+        animation-timing-function: ease-out;
+        opacity: 85%;
+    }
+    .img-hero, .img-cape {
+        position: absolute;
+        display: block;
         width: 100%;
-        height: auto;
+        height: 70vh;
+        object-fit: cover;
+        object-position: 50% 20%;
+    }
+
+    @keyframes anim-cape-opacity {
+        0%   {opacity: 0%;}
+        100% {opacity: 60%;}
+    }
+    
+    .img-cape {
+        opacity: 60%;
+        filter: url("#turbulence");
+        animation-name: anim-cape-opacity;
+        animation-duration: 5s;
+        animation-timing-function: ease-out;
     }
 </style>
 
 <div>
-    <img src="{ imgSrc }" alt="{ imgAlt }">
-    <h1>{ title }</h1>
+    <img class="img-hero" src="{ imgSrc }" alt="{ imgAlt }">
+    <img class="img-cape" src="{ imgSrc }" alt="" role="presentation">
+    <h1 class="title">{ title }</h1>
+
+    <svg>
+        <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+            <feTurbulence 
+                id="sea-filter" 
+                numOctaves="3" 
+                seed="2" 
+                baseFrequency="0.02 0.05">
+            </feTurbulence>
+            <feDisplacementMap 
+                scale="20" 
+                in="SourceGraphic">
+            </feDisplacementMap>
+            <animate 
+                xlink:href="#sea-filter" 
+                attributeName="baseFrequency" 
+                dur="60s" 
+                keyTimes="0;0.5;1" 
+                values="0.02 0.06;0.04 0.08;0.02 0.06" 
+                repeatCount="indefinite"/>
+        </filter>
+    </svg>
+
+    <animate xlink:href="#sea-filter" attributeName="baseFrequency" dur="60s" keyTimes="0;0.5;1" values="0.02 0.06;0.04 0.08;0.02 0.06" repeatCount="indefinite"/>
 </div>
