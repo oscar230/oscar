6dc8cc3a45377c8a87a58c5d8013e777c3fbc0a7
Oscar Andersson <oscar.andersson.200@gmail.com>
2021-02-15 23:24:25 +0100

    Smaller font in slip.

--- src/Slip.svelte
+++ src/Slip.svelte
@@ -10,7 +10,7 @@
         left: 46vw;
         width: 45vw;
         height: 70vh;
-        font-size: 2.5vw;
+        font-size: 2.3vw;
         color: #ffc048;
         text-shadow: 1px 1px #ffa801, 2px 2px #ffa801;
         opacity: 80%;
