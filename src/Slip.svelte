1153198728458b01169ae9a35cb49d9546ede963
Oscar Andersson <oscar.andersson.200@gmail.com>
2021-02-15 23:20:34 +0100

    Slip component added, adds text.

---
+++ src/Slip.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let htmlContent;
+</script>
+
+<style>
+    .content {
+        display: block;
+        position: absolute;
+        top: 4vh;
+        left: 46vw;
+        width: 45vw;
+        height: 70vh;
+        font-size: 2.5vw;
+        color: #ffc048;
+        text-shadow: 1px 1px #ffa801, 2px 2px #ffa801;
+        opacity: 80%;
+    }
+</style>
+
+<div>
+    <span class="content">{ @html htmlContent }</span>
+</div>
