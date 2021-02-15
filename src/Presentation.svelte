21a76559d2f8c67d8dda3efa48c42bee7d859855
Oscar Andersson <oscar.andersson.200@gmail.com>
2021-02-15 16:55:43 +0100

    adds presentation comp.

---
+++ src/Presentation.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let imgSrc;
+    export let imgAlt;
+    export let title;
+</script>
+
+<style>
+    div {
+        margin: auto;
+		width: 50%;
+    }
+    h1 {
+        position: relative;
+        top: -5em;
+        left: -5em;
+        font-size: larger;
+    }
+    img {
+        width: 100%;
+        height: auto;
+    }
+</style>
+
+<div>
+    <img src="{ imgSrc }" alt="{ imgAlt }">
+    <h1>{ title }</h1>
+</div>
