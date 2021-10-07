89a825eca5f31af317d0b98f4a1cf1389b35da01
Oscar Andersson <oscar.andersson.200@gmail.com>
2021-10-07 22:29:19 +0200

    Adds build blog in site build

--- build-site.sh
+++ build-site.sh
@@ -2,6 +2,7 @@
 SRCDIR="src"
 OUTDIR="out"
 PAGES=$SRCDIR"/pages/*"
+BLOGPOSTS=$SRCDIR"/blog-posts/*"
 
 rm -rf $OUTDIR/*
 mkdir -p $OUTDIR
@@ -9,6 +10,11 @@ mkdir -p $OUTDIR
 cp -r $SRCDIR/resources/ $OUTDIR
 cp -r $SRCDIR/styles/ $OUTDIR
 cp $SRCDIR/* $OUTDIR 2>/dev/null
+
+for blogpost in $BLOGPOSTS; do
+    [ -e "$blogpost" ] || continue
+    sh build-blog-post.sh $SRCDIR $OUTDIR $(realpath $blogpost)
+done
 
 for page in $PAGES; do
     [ -e "$page" ] || continue
