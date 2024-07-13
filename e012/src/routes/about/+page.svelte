92d6f664c6df5db985e09467d70cae1cb6dfd143
Oscar Andersson <oscaande@outlook.com>
2024-07-13 14:39:06 +0200

    Add fonts and licenses

--- e012/src/routes/about/+page.svelte
+++ e012/src/routes/about/+page.svelte
@@ -23,4 +23,21 @@
 		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
 		using it with JavaScript disabled!
 	</p>
+	<h2>Font Licenses</h2>
+    <p>E012 uses the following fonts under respective licenses:</p>
+    <ul>
+        <li>
+            <p><strong>Elegant Typewriter</strong> Regular, Bold, Light<br>free for non-commercial use only by Lukas Krakora 2014.<br>Paypal donation <a href="mailto:krraaa@yahoo.com">krraaa@yahoo.com</a>.</p>
+        </li>
+        <li>
+            <p><strong>Another Typewriter</strong><br>Freeware by Johan Holmdahl 1999.</p>
+        </li>
+        <li>
+			<p><strong>JMH Typewriter</strong> Regular, Black, Bold, Thin<br>Free for personal use only by jorge Morón.<br>Contact <a href="mailto:joorgemoron@gmail.com">joorgemoron@gmail.com</a>.</p>
+        </li>
+        <li>
+			<p>
+            <strong>Volta</strong> Bold V2, Medium Italic V2, Medium V2, Regular<br>Credited to <a href="http://www.onlinewebfonts.com" target="_blank">Web Fonts</a>.</p>
+        </li>
+    </ul>
 </div>
