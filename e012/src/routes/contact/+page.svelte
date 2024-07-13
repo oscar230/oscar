3417882b25dbf1b6e424f5b1783ed5ad0fbb7199
Oscar Andersson <oscaande@outlook.com>
2024-07-13 21:01:54 +0200

    Fix contact page

--- e012/src/routes/contact/+page.svelte
+++ e012/src/routes/contact/+page.svelte
@@ -6,9 +6,9 @@
 <div class="text-column">
     <h1>Contact</h1>
     <ul>
-        <li>Instagram: <a href="https://www.instagram.com/oscar_erik_andersson/" target="_blank" rel="noopener noreferrer"></a></li>
-        <li>Chat: <a href="https://matrix.to/#/@oscarandersson:matrix.org" target="_blank" rel="noopener noreferrer">@oscarandersson:matrix.org</a></li>
-        <li>Email: <a href="mailto:webmasterwonky@duck.com" target="_blank" rel="noopener noreferrer">webmasterwonky@duck.com</a></li>
+        <li>Instagram: <a href="https://www.instagram.com/oscar_erik_andersson/" target="_blank" rel="noopener noreferrer">oscar_erik_andersson</a>.</li>
+        <li>Chat: <a href="https://matrix.to/#/@oscarandersson:matrix.org" target="_blank" rel="noopener noreferrer">@oscarandersson:matrix.org</a>.</li>
+        <li>Email: <a href="mailto:webmasterwonky@duck.com" target="_blank" rel="noopener noreferrer">webmasterwonky@duck.com</a>.</li>
     </ul>
     <br />
     <p>AGE Public key:<br><code>age1k5hg5g43x3pvxy3qylarkq90xxmulnugynm5w5vy0s47hhkdgg5qdwx9k8</code></p>
