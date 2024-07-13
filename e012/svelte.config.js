e6a5e7e176d44a0e73c6ea1017c456801fad468d
Oscar Andersson <oscaande@outlook.com>
2024-07-13 22:41:04 +0200

    Update svellte to build SSR

--- e012/svelte.config.js
+++ e012/svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
 	preprocess: vitePreprocess(),
