3afe928e70fd6689992189d5fcaaf32f71729514
Oscar Andersson <oscaande@outlook.com>
2024-07-13 23:04:17 +0200

    Update contact page

--- e012/src/routes/contact/+page.svelte
+++ e012/src/routes/contact/+page.svelte
@@ -5,11 +5,12 @@
 
 <div class="text-column">
     <h1>Contact</h1>
+    <p>For DJ-related questions or bookings you can never go wrong with a DM on my Instagram.</p>
     <ul>
-        <li>Instagram: <a href="https://www.instagram.com/oscar_erik_andersson/" target="_blank" rel="noopener noreferrer">oscar_erik_andersson</a>.</li>
-        <li>Chat: <a href="https://matrix.to/#/@oscarandersson:matrix.org" target="_blank" rel="noopener noreferrer">@oscarandersson:matrix.org</a>.</li>
-        <li>Email: <a href="mailto:webmasterwonky@duck.com" target="_blank" rel="noopener noreferrer">webmasterwonky@duck.com</a>.</li>
+        <li>Instagram: <a href="https://www.instagram.com/oscar_erik_andersson/" target="_blank" rel="noopener noreferrer">oscar_erik_andersson</a></li>
+        <li>Chat: <a href="https://matrix.to/#/@oscarandersson:matrix.org" target="_blank" rel="noopener noreferrer">@oscarandersson:matrix.org</a></li>
+        <li>Email: <a href="mailto:e012@oscaande.mozmail.com" target="_blank" rel="noopener noreferrer">e012@oscaande.mozmail.com</a></li>
     </ul>
-    <br />
+    <p><b>Note</b> that <i>e012@oscaande.mozmail.com</i> is a proxy address, it blocks spam and promotional emails. Send something nice like "Hi whats up" and I will get back to you with my real email address in my reply. Thanks.</p>
     <p>AGE Public key:<br><code>age1k5hg5g43x3pvxy3qylarkq90xxmulnugynm5w5vy0s47hhkdgg5qdwx9k8</code></p>
 </div>
