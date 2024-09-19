52d4412dc00f830e24c94abe8c438adfd52d80ce
Oscar Andersson <oscaande@outlook.com>
2024-09-19 09:24:40 +0200

    Update +page.svelte

--- e012/src/routes/about/+page.svelte
+++ e012/src/routes/about/+page.svelte
@@ -12,23 +12,27 @@
 	<p>E012.se, no rights reserved! <img src="{kopimi}" alt="The logotype of Kopimi" class="kopimi"></p>
 	<h2>Host</h2>
 	<p>E012.se hosts some projects on it's servers, these are either projects that I like or that I'm a part of. If you would like to host your project here or have any legal comments regarding the projects, please <a href="/contact">contact me</a>. Some of the projects include <a href="https://grupp054.se/" target="_blank" rel="noopener noreferrer">Grupp 054</a>, <a href="https://klanglandet.se/" target="_blank" rel="noopener noreferrer">Klanglandet</a> and <a href="https://oresundspacecollective.com/" target="_blank" rel="noopener noreferrer">Øresund Space Collective</a>.</p>
+	
+	<h2>Contribute to the Tor Network</h2>
+	<p>Enable this Snowflake and leave the tab open to help internet users in oppressive regimes by routing traffic trough your browser into the Tor Network. Thanks. <3</p>
+	<iframe src="https://snowflake.torproject.org/embed.html" width="320" height="240" frameborder="0" scrolling="no"></iframe>
+	<br/>	
 	<h2>Used licenses</h2>
-    <p>This website uses the following fonts under respective licenses:</p>
-    <ul>
-        <li>
-            <p><strong>Elegant Typewriter</strong> Regular, Bold, Light<br>free for non-commercial use only by Lukas Krakora 2014.<br>Paypal donation <a href="mailto:krraaa@yahoo.com">krraaa@yahoo.com</a>.</p>
-        </li>
-        <li>
-            <p><strong>Another Typewriter</strong><br>Freeware by Johan Holmdahl 1999.</p>
-        </li>
-        <li>
+	    <p>This website uses the following fonts under respective licenses:</p>
+	    <ul>
+	        <li>
+	            	<p><strong>Elegant Typewriter</strong> Regular, Bold, Light<br>free for non-commercial use only by Lukas Krakora 2014.<br>Paypal donation <a href="mailto:krraaa@yahoo.com">krraaa@yahoo.com</a>.</p>
+	        </li>
+	        <li>
+	            	<p><strong>Another Typewriter</strong><br>Freeware by Johan Holmdahl 1999.</p>
+	        </li>
+	        <li>
 			<p><strong>JMH Typewriter</strong> Regular, Black, Bold, Thin<br>Free for personal use only by jorge Morón.<br>Contact <a href="mailto:joorgemoron@gmail.com">joorgemoron@gmail.com</a>.</p>
-        </li>
-        <li>
-			<p>
-            <strong>Volta</strong> Bold V2, Medium Italic V2, Medium V2, Regular<br>Credited to <a href="http://www.onlinewebfonts.com" target="_blank">Web Fonts</a>.</p>
-        </li>
-    </ul>
+	        </li>
+	        <li>
+		<p><strong>Volta</strong> Bold V2, Medium Italic V2, Medium V2, Regular<br>Credited to <a href="http://www.onlinewebfonts.com" target="_blank">Web Fonts</a>.</p>
+		</li>
+	</ul>
 	<p>There are multiple open source packages used, <a href="https://raw.githubusercontent.com/oscar230/oscar/master/e012/package.json" target="_blank" rel="noopener noreferrer">avaliable here</a>.</p>
 </div>
 
